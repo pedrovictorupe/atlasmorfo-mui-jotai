02f74d090d1b6e88dfe4ce0ac45b97328ad77d8a
Pedro Cordeiro <pedro.victorbrito@upe.br>
2022-11-30 10:13:24 -0300

    Adiciona o texto Prosseguir em NextTabButton

--- src/components/NextTabButton.tsx
+++ src/components/NextTabButton.tsx
@@ -3,6 +3,6 @@ import DefaultButton from "./DefaultButton";
 
 export default (props: { onClick: () => void }) => (
   <div style={{ textAlign: "right" }}>
-    <DefaultButton onClick={props.onClick} />
+    <DefaultButton onClick={props.onClick}>Prosseguir</DefaultButton>
   </div>
 );
