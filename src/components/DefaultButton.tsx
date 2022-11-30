06f15a9cf5bc8b90a743542c31cdbd75fc99b121
Pedro Cordeiro <pedro.victorbrito@upe.br>
2022-11-30 10:09:33 -0300

    Implementa a capacidade de editar o texto de DefaultButton

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -2,13 +2,13 @@ import { ArrowForward } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import React from "react";
 
-export default (props: { onClick: () => void }) => (
+export default (props: { onClick: () => void; children: string }) => (
   <Button
     variant="contained"
     startIcon={<ArrowForward />}
     sx={{ borderRadius: 10 }}
     onClick={props.onClick}
   >
-    Prosseguir{" "}
+    {props.children}
   </Button>
 );
