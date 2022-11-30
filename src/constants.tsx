f671422aab819f1e38fd9fb8fbbefd36bd50b26a
Pedro Cordeiro <pedro.victorbrito@upe.br>
2022-11-30 19:44:29 -0300

    Remove constantes inúteis

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,18 +1,6 @@
 import DefaultPage from "./@types/DefaultPage";
-import LessonStateEnum from "./@types/LessonStateEnum";
-import content from "./contents.json";
-
-export const StatesPossiveis: LessonStateEnum[] = [
-  "INTRO_LIDA",
-  "PRETESTE_RESPONDIDO",
-  "GABARITO_PRETESTE_VISUALIZADO",
-  "MUDANDO_RESPOSTA",
-  "POS_TESTE_RESPONDIDO",
-];
 
 export const DEFAULT_PAGES: DefaultPage[] = ["INTRO", "PRE", "VIDEO", "POS"];
-
-export const LESSON_NAMES = Object.keys(content);
 
 export const DRAWER_WIDTH = 240;
 
