cb443e722e7d6e24df9c05e23a346fc6d738ce1a
Pedro Cordeiro <pedro.victorbrito@upe.br>
2022-11-30 10:17:43 -0300

    Implementa flexibilidade de ícones em DefaultButton

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -2,10 +2,14 @@ import { ArrowForward } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import React from "react";
 
-export default (props: { onClick: () => void; children: string }) => (
+export default (props: {
+  onClick: () => void;
+  children: string;
+  startIcon?: React.ReactNode;
+}) => (
   <Button
     variant="contained"
-    startIcon={<ArrowForward />}
+    startIcon={props.startIcon}
     sx={{ borderRadius: 10 }}
     onClick={props.onClick}
   >
--- src/components/NextTabButton.tsx
+++ src/components/NextTabButton.tsx
@@ -1,8 +1,11 @@
+import { ArrowForward } from "@mui/icons-material";
 import React from "react";
 import DefaultButton from "./DefaultButton";
 
 export default (props: { onClick: () => void }) => (
   <div style={{ textAlign: "right" }}>
-    <DefaultButton onClick={props.onClick}>Prosseguir</DefaultButton>
+    <DefaultButton onClick={props.onClick} startIcon={<ArrowForward />}>
+      Prosseguir
+    </DefaultButton>
   </div>
 );
