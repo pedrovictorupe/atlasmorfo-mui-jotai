734aecb74a1aa756d8cb41bbb54485c7177cceb7
Pedro Cordeiro <pedro.victorbrito@upe.br>
2022-11-30 21:34:03 -0300

    Inclui um componente esquecido no último commit

---
+++ src/components/PreTestePostConfirmDialog.tsx
@@ -0,0 +1,36 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import React, { useState } from "react";
+import LessonStateEnum from "../@types/LessonStateEnum";
+
+export default ({ lessonState }: DialogProps) => {
+  const [isExplanationDialogOpen, setExplanationDialogOpen] = useState(true);
+
+  return (
+    <Dialog
+      open={isExplanationDialogOpen && lessonState == "PRETESTE_RESPONDIDO"}
+      onClose={() => {
+        setExplanationDialogOpen(false);
+      }}
+    >
+      <DialogContent>
+        <DialogContentText>
+          Sua resposta não será checada imediatamente. Você terá a chance de
+          mudá-la após assistir ao vídeo da próxima sessão.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setExplanationDialogOpen(false)}>OK</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+type DialogProps = {
+  lessonState: LessonStateEnum;
+};
