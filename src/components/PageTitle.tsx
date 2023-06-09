9b24eeacabc1ca050f75922efbc00c82420b5b15
Pedro Victor <pedrocga1@gmail.com>
2023-06-08 23:36:51 -0300

    Diminui o heading dos títulos

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,12 +2,7 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 export default ({ children }: any) => (
-  <Typography
-    variant="h3"
-    padding={2}
-    letterSpacing={4}
-    fontWeight={1}
-  >
+  <Typography variant="h4" padding={2} letterSpacing={4} fontWeight={1}>
     {children}
   </Typography>
 );
